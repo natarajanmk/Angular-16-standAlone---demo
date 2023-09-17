6318f4e9d07232229af5938ab06fd696893245d4
natarajanmk <natarajanmk@gmail.com>
2023-09-17 13:00:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-16-standAlone---demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-1wp4ea)
---
+++ src/app/app.route.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+
+export const APP_ROUTE: Route[] = [
+  { path: '', component: BlogComponent },
+  {
+    path: '',
+    loadChildren: () => import('./blog/blog.route').then((m) => m.BLOG_ROUTE),
+  },
+];
